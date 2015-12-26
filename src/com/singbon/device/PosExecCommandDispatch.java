c8d644a48f11a69c44ee6693c92379f3f6086a8e
ycsty2008 <594973849@qq.com>
2015-12-26 11:53:13 +0800

    black download

--- src/com/singbon/device/PosExecCommandDispatch.java
+++ src/com/singbon/device/PosExecCommandDispatch.java
@@ -45,7 +45,8 @@ public class PosExecCommandDispatch {
 		// 心跳包
 		if (b[30] == PosFrame.Status && b[31] == PosSubFrameStatus.SysStatus) {
 			// 监控打开执行操作
-			if (TerminalManager.CompanyIdToMonitorRunningList.containsKey(device.getCompanyId()) && TerminalManager.CompanyIdToMonitorRunningList.get(device.getCompanyId())) {
+			if (TerminalManager.CompanyIdToMonitorRunningList.containsKey(device.getCompanyId())
+					&& TerminalManager.CompanyIdToMonitorRunningList.get(device.getCompanyId())) {
 				map.put("'type'", "s");
 				// 非中转设备
 				if (device.getDeviceType() != 1) {
@@ -90,19 +91,23 @@ public class PosExecCommandDispatch {
 	private static void autoExecCommand(InetSocketAddress inetSocketAddress, byte[] b, Device device, int commandCode) {
 		// 相差半分钟校时
 		Calendar c1 = Calendar.getInstance();
-		c1.set(StringUtil.objToInt("20" + StringUtil.getHexStrFromBytes(45, 45, b)), StringUtil.objToInt(StringUtil.getHexStrFromBytes(46, 46, b)),
-				StringUtil.objToInt(StringUtil.getHexStrFromBytes(47, 47, b)), StringUtil.objToInt(StringUtil.getHexStrFromBytes(48, 48, b)),
-				StringUtil.objToInt(StringUtil.getHexStrFromBytes(49, 49, b)), StringUtil.objToInt(StringUtil.getHexStrFromBytes(50, 50, b)));
+		c1.set(StringUtil.objToInt("20" + StringUtil.getHexStrFromBytes(45, 45, b)),
+				StringUtil.objToInt(StringUtil.getHexStrFromBytes(46, 46, b)),
+				StringUtil.objToInt(StringUtil.getHexStrFromBytes(47, 47, b)),
+				StringUtil.objToInt(StringUtil.getHexStrFromBytes(48, 48, b)),
+				StringUtil.objToInt(StringUtil.getHexStrFromBytes(49, 49, b)),
+				StringUtil.objToInt(StringUtil.getHexStrFromBytes(50, 50, b)));
 		c1.add(Calendar.MONTH, -1);
 		Calendar c2 = Calendar.getInstance();
 		c2.setTime(new Date());
 
 		if (Math.abs(c1.getTimeInMillis() - c2.getTimeInMillis()) > 30000) {
-			String sendBufStr = StringUtil.hexLeftPad(PosFrame.Sys07, 2) + StringUtil.hexLeftPad(PosSubFrameSys07.SysTime, 2) + "0000" + StringUtil.hexLeftPad(commandCode, 4)
-					+ StringUtil.timeToHexStr() + "0000";
+			String sendBufStr = StringUtil.hexLeftPad(PosFrame.Sys07, 2)
+					+ StringUtil.hexLeftPad(PosSubFrameSys07.SysTime, 2) + "0000"
+					+ StringUtil.hexLeftPad(commandCode, 4) + StringUtil.timeToHexStr() + "0000";
 			String bufLen = StringUtil.hexLeftPad(2 + sendBufStr.length() / 2, 4);
-			sendBufStr = device.getSn() + StringUtil.hexLeftPad(device.getDeviceNum(), 8) + CommandDevice.NoSubDeviceNum + DeviceType.Main + DeviceType.getDeviceTypeFrame(device) + bufLen
-					+ sendBufStr;
+			sendBufStr = device.getSn() + StringUtil.hexLeftPad(device.getDeviceNum(), 8) + CommandDevice.NoSubDeviceNum
+					+ DeviceType.Main + DeviceType.getDeviceTypeFrame(device) + bufLen + sendBufStr;
 			byte[] sendBuf = StringUtil.strTobytes(sendBufStr);
 			try {
 				TerminalManager.sendToPos(inetSocketAddress, sendBuf);
@@ -111,24 +116,25 @@ public class PosExecCommandDispatch {
 			}
 		}
 
-		int lastBatchId = Integer.parseInt(StringUtil.getHexStrFromBytes(38, 39, b), 16);
-		long lastBlackNum = Long.parseLong(StringUtil.getHexStrFromBytes(40, 43, b), 16);
 		// 自动下载批次黑名单
+		int lastBatchId = Integer.parseInt(StringUtil.getHexStrFromBytes(38, 39, b), 16);
 		long sysLastBatchId = 0;
 		if (TerminalManager.CompanyIdToLastBatchIdList.containsKey(device.getCompanyId())) {
 			sysLastBatchId = TerminalManager.CompanyIdToLastBatchIdList.get(device.getCompanyId());
 		}
-		long sysLastBlackNum = 0;
-		if (TerminalManager.CompanyIdToLastBlackNumList.containsKey(device.getCompanyId())) {
-			sysLastBlackNum = TerminalManager.CompanyIdToLastBlackNumList.get(device.getCompanyId());
-		}
-		if (lastBatchId != sysLastBatchId) {
+		if (lastBatchId != sysLastBatchId && sysLastBatchId != 0) {
 			PosExecBatchBlack black = new PosExecBatchBlack(lastBatchId, device);
 			black.run();
 		}
 
 		// 自动下载黑名单
-		if (lastBlackNum != sysLastBlackNum) {
+		long lastBlackNum = Long.parseLong(StringUtil.getHexStrFromBytes(40, 43, b), 16);
+		long sysLastBlackNum = 0;
+		if (TerminalManager.CompanyIdToLastBlackNumList.containsKey(device.getCompanyId())) {
+			sysLastBlackNum = TerminalManager.CompanyIdToLastBlackNumList.get(device.getCompanyId());
+		}
+
+		if (lastBlackNum != sysLastBlackNum && sysLastBlackNum != 0) {
 			PosExecCardBlack black = new PosExecCardBlack(lastBlackNum, device);
 			black.run();
 		}
